fbd6944b05e72f8c560c6fdd57ce604712ed22d4
Honza Bambas :mayhemer <honzab@firemni.cz>
2019-07-11 22:21:02 +0200

    don't kill the h1 client socket so aggressively + nits

--- proxy.js
+++ proxy.js
@@ -336,7 +336,7 @@ function handle_h1_connect(client_request, client_socket, head) {
   const auth_value = client_request.headers.host;
 
   console.log('CONNECT\'ing (HTTP/1) to', auth_value, 'proxy>h1 port:', client_request.socket.remotePort);
-  console.log('  *** NEW HTTP/1 TUNNEL', '( h1-tunnels:', ++h1_tunnel_count, 'sessions:', session_count, ')');
+  console.log('*** NEW HTTP/1 TUNNEL', '( h1-tunnels:', ++h1_tunnel_count, 'sessions:', session_count, ')');
 
   // Just for testing how the client behaves when authentication is required
   if (!authenticated(client_request.headers)) {
@@ -398,7 +398,7 @@ function handle_h1_connect(client_request, client_socket, head) {
   });
   client_socket.on('close', () => {
     console.log('HTTP/1 connect tunnel closed', auth_value);
-    console.log('  *** CLOSED HTTP/1 TUNNEL', '( h1-tunnels:', --h1_tunnel_count, 'sessions:', session_count, ')');
+    console.log('*** CLOSED HTTP/1 TUNNEL', '( h1-tunnels:', --h1_tunnel_count, 'sessions:', session_count, ')');
     server_socket.end();
   });
 }
@@ -452,8 +452,6 @@ function handle_h1_non_connect(client_request, client_response) {
       server_response.on('end', () => {
         console.log('RESPONSE (HTTP/1) END', url);
         client_response.end();
-        // Explicitely close the socket, out HTTP/1 request connections can't be resused
-        client_response.socket.destroy();
         server_response.destroy();
       });
     } catch (exception) {
